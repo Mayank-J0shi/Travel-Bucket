223b788b8adf1f352729145cfe5c6425db505fcb
Mayank Joshi <mayank2000j@gmail.com>
2022-02-14 23:20:35 +0530

    MongoDB Models in Place

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+//Satyavachan
+//Models naming is generally Capitalized and singular
+//Routes naming is small letters and plural
+//user.save() takes time like it connects to db and then try to save the user so that might take time so we convert it to async function
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+app.use(express.json());
+
+
+
+//connection to db
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
+
+app.listen(process.env.PORT, (req,res) => {
+  console.log("Backend server running at 8800 port");
+});
+
+
+
+
+
+
+
+
+
+
